47be3fba33a790fe5f0e27f5b5f12fc5c8173f62
alsbtain <info@alsbtain.net>
2016-06-27 14:17:25 -0700

    sBulletin Media

---
+++ www/settings.js
@@ -0,0 +1,14 @@
+var siteURL = 'http://www.nahawnd.net/';
+
+var shareURL = siteURL + 'media_redirect.php';
+
+var externalMenu = { 
+    title: ["الموقع الرسمي"],
+    url: ["http://www.nahawnd.net/"]
+};
+
+var copyRight = 'nahawnd.net';
+
+var shareFooter = "\\n\\nمن تطبيق شبكة نهاوند الولائية";
+
+var AndroidSenderID = "141638044342";
